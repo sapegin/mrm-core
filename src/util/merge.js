a236c065e50cbb2e231c71d6868d43f8bc63449c
Artem Sapegin <artem@sapegin.ru>
2017-12-20 09:21:43 +0100

    Chore: Add a comment

--- src/util/merge.js
+++ src/util/merge.js
@@ -3,6 +3,7 @@
 const mergeBase = require('webpack-merge');
 const _ = require('lodash');
 
+/** Deep merge that merge arrays without duplicates */
 const merge = mergeBase({
 	customizeArray: (a, b) => a.concat(_.differenceWith(b, a, _.isEqual)),
 });
